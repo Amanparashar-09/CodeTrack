4c06b9ae05127e0b20760468b7f3ad7a4a6f3706
Amanparashar-09 <amanparashar0911@gmail.com>
2026-03-25 17:10:59 +0530

    feat: add global error handler

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { notFound, errorHandler } = require("./middleware/errorHandler");
 
 const app = express();
 
@@ -7,5 +8,8 @@ app.use(express.json());
 app.get("/health", (_req, res) => {
   res.status(200).json({ message: "OK" });
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
