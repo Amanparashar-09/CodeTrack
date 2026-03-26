f73043ce833609caca79d87cb688d9dbf6f1e2b4
Amanparashar-09 <amanparashar0911@gmail.com>
2026-03-26 19:41:50 +0530

    feat: add points system

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -14,6 +14,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    totalPoints: {
+      type: Number,
+      default: 0,
+    },
   },
   {
     timestamps: true,
